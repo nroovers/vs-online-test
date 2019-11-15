cb205d3cfe9799d88b7dc16bf247ca491b630ab6
ROOVERS Nicolai <Nicolai.ROOVERS@echa.europa.eu>
2019-11-15 10:00:58 +0000

    Initial commit

---
+++ someapp/src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <header>
+        <h2>
+          VS Online app.
+        </h2>
+      </header>
+      <p>
+      This is a new React app hosted on VS Online...
+      </p>
+      <p>
+        balbalbalba
+      </p>
+    </div>
+  );
+}
+
+export default App;
